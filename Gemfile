bccfb9e4ed3d2a62b5acf1c414fccf49cc258249
Chris Alley <chris@chrisalley.info>
2026-01-20 18:44:52 +1300

    Update schema to Rails 8.1 format and remove unused dependencies

--- Gemfile
+++ Gemfile
@@ -8,25 +8,6 @@ gem "sprockets-rails"
 gem "sqlite3", ">= 1.4"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
-# Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
-gem "importmap-rails"
-# Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
-gem "turbo-rails"
-# Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
-gem "stimulus-rails"
-# Build JSON APIs with ease [https://github.com/rails/jbuilder]
-gem "jbuilder"
-# Use Redis adapter to run Action Cable in production
-# gem "redis", ">= 4.0.1"
-
-# Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
-# gem "kredis"
-
-# Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
-
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem "tzinfo-data", platforms: %i[ windows jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_18_105647) do
+ActiveRecord::Schema[8.1].define(version: 2024_10_18_105647) do
   create_table "blogs", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.text "body"
-    t.boolean "publish"
-    t.integer "user_id", null: false
     t.integer "blog_id", null: false
+    t.text "body"
     t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
+    t.boolean "publish"
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["blog_id"], name: "index_posts_on_blog_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "administrator", default: false
+    t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
     t.datetime "updated_at", null: false
-    t.boolean "administrator", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
